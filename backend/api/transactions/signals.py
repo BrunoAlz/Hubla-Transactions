5c166ecfc0441832b4e26d7e8915d8a8b34b2141
Bruno Alves <bruno6821@gmail.com>
2023-04-26 17:24:57 -0300

    Refactor: trocando Model do siginals

--- backend/api/transactions/signals.py
+++ backend/api/transactions/signals.py
@@ -1,12 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Transaction
+from .models import Contract
 
 from core.utils.data_process import process_file
 import os
 
 
-@receiver(post_save, sender=Transaction)
+@receiver(post_save, sender=Contract)
 def process_data(sender, instance, **kwargs):
     print("CHAMANDO O SIGNAL PARA INICIAR O PROCESSAMENTO!")
     path = instance.upload.path
