6d1128635e6b41c7dcbe2e4bf4eebb7e50bfb6f9
Bruno Alves <bruno6821@gmail.com>
2023-04-26 00:45:41 -0300

    Validando Password no Registro

--- backend/administrator/views.py
+++ backend/administrator/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import exceptions
+from common.serializers import UserSerializer
 
 
 class RegisterUserView(APIView):
     def post(self, request):
-        # Testar api
+        #validando os dados do registro
+        data = request.data
+        # compara se os passwords são iguais
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Passwords do not match!')
+        
         return Response('ok')
