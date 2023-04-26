dddaca096426106929e4a6d64c9346244ed71f95
Bruno Alves <bruno6821@gmail.com>
2023-04-26 17:24:37 -0300

    Implementando o Signals para rodar as transações

---
+++ backend/api/transactions/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Transaction
+
+from core.utils.data_process import process_file
+import os
+
+
+@receiver(post_save, sender=Transaction)
+def process_data(sender, instance, **kwargs):
+    print("CHAMANDO O SIGNAL PARA INICIAR O PROCESSAMENTO!")
+    path = instance.upload.path
+    if os.path.isfile(path):
+        process_file(path)
