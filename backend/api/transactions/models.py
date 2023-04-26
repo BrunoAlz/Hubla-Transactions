c7f530feb9815538d5f29396b6f727d631a8db06
Bruno Alves <bruno6821@gmail.com>
2023-04-26 15:30:52 -0300

    Implementando os Models TType e Transaction

--- backend/api/transactions/models.py
+++ backend/api/transactions/models.py
@@ -25,3 +25,85 @@ class Contract(models.Model):
 
     def __str__(self):
         return self.creator.first_name
+
+
+class TransactionType(models.Model):
+
+    NATURE = (
+        ("Entrada", "Entrada"),
+        ("Saida", "Saída"),
+    )
+
+    SIGNAL = (
+        ("+", "+"),
+        ("-", "-"),
+    )
+
+    type = models.AutoField(
+        primary_key=True,
+        editable=False
+    )
+
+    description = models.CharField(
+        'Description',
+        max_length=20
+    )
+
+    nature = models.CharField(
+        max_length=20,
+        choices=NATURE,
+        default="Entrada"
+    )
+
+    signal = models.CharField(
+        max_length=1,
+        choices=SIGNAL,
+        default=" "
+    )
+
+    class Meta:
+        verbose_name = ("TransactionType")
+        verbose_name_plural = ("TransactionTypes")
+
+    def __str__(self):
+        return f'{self.type} - {self.description} - {self.nature} - {self.signal}'
+
+    def get_absolute_url(self):
+        return reverse("TransactionType_detail", kwargs={"pk": self.pk})
+
+
+class Transaction(models.Model):
+
+    type = models.ForeignKey(
+        TransactionType,
+        on_delete=models.DO_NOTHING
+    )
+
+    date = models.DateTimeField(
+        'date',
+    )
+
+    product = models.CharField(
+        'product',
+        max_length=30,
+    )
+
+    price = models.PositiveIntegerField(
+        null=True, blank=True
+    )
+
+    seller = models.CharField(
+        'seller',
+        max_length=20,
+        null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = ("Transaction")
+        verbose_name_plural = ("Transactions")
+
+    def __str__(self):
+        return f'{self.type} - {self.date} - {self.product} - {self.price} - {self.seller}'
+
+    def get_absolute_url(self):
+        return reverse("Transaction_detail", kwargs={"pk": self.pk})
