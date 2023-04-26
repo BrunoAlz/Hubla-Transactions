e3f62a0efa8904d022c71cc4eae4815be41e155c
Bruno Alves <bruno6821@gmail.com>
2023-04-26 07:11:52 -0300

    Criando o model Contract para as Transactions

--- backend/api/transactions/models.py
+++ backend/api/transactions/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Contract(models.Model):
+
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    upload = models.FileField(
+        'Document'
+    )
+
+    class Meta:
+        verbose_name = "Contract"
+        verbose_name_plural = "Contracts"
+
+    def __str__(self):
+        return self.creator
+
+    def get_absolute_url(self):
+        return reverse("Contract_detail", kwargs={"pk": self.pk})
