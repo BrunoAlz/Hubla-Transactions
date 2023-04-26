4989eca8c35cf984b211886596d66f802ab634a4
Bruno Alves <bruno6821@gmail.com>
2023-04-26 02:16:49 -0300

    Ajustes de Nomes, conforme a Documentação

--- backend/administrator/authentication.py
+++ backend/administrator/authentication.py
@@ -4,15 +4,18 @@ class JWTAuthentucation:
     """
     Criando o JWT utilizando a Lib: https://pyjwt.readthedocs.io/en/stable/
     """
+    # Método para gerar o TOKEN 
     def generate_jwt(self, id):
         #Cria o Payload para montar o JTW
         payload = {
             # Passa o Id do Usuário
             'user_id': id,
             # Passa a Data de Expiração
-            'expire_time': datetime.datetime.utcnow() + datetime.timedelta(days=1),
+            # “exp” (Expiration Time) Claim
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1),
             # passa a outra data
-            'date': datetime.datetime.utcnow(),
+            # “iat” (Issued At) Claim
+            'iat': datetime.datetime.utcnow(),
         }
 
         # CRIAR A CHAVE SECRETA NO .env!!!!
