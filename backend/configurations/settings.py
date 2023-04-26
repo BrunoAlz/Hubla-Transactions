738646a57ab63ea0357c6e285c56696bf28e75ca
Bruno Alves <bruno6821@gmail.com>
2023-04-26 05:05:56 -0300

    Refactor na Estrutura do Projeto

--- backend/configurations/settings.py
+++ backend/configurations/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import sys
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.insert(0, os.path.join(BASE_DIR, 'api'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -38,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'core',
-    'administrator',
     'corsheaders',
+    'api.administrator',
+    'api.core',
 ]
 
 MIDDLEWARE = [
@@ -101,7 +103,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     # https://www.django-rest-framework.org/api-guide/authentication/#tokenauthentication
-    'DEFAULT_AUTHENTICATION_CLASSES': (        
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     )
 }
