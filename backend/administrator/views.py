36b2bb429505c4950089a1cafaae0293764eaf0b
Bruno Alves <bruno6821@gmail.com>
2023-04-26 04:10:26 -0300

    Ajustes de PEP8

--- backend/administrator/views.py
+++ backend/administrator/views.py
@@ -10,18 +10,20 @@ from .authentication import JWTAuthentucation
 from rest_framework import permissions
 
 # Refatorar para convenção do DRF
+
+
 class RegisterUserView(APIView):
     """
     Esta view permite a criação de usuários no sistema.
     """
+
     def post(self, request):
-        #validando os dados do registro
+        # validando os dados do registro
         data = request.data
         # compara se os passwords são iguais
         if data['password'] != data['password_confirm']:
             # Criando Exceções customizadas
             raise api_exceptions.PasswordDoNotMatchException()
-        
 
         serializer = UserSerializer(data=data)
         serializer.is_valid(raise_exception=True)
@@ -40,24 +42,25 @@ class LoginUserAPIView(APIView):
 
         # Verifica se o usuário está correto / existe
         if user is None:
-            raise exceptions.AuthenticationFailed({'error': 'Please, verify your data input'})
-        
+            raise exceptions.AuthenticationFailed(
+                {'error': 'Please, verify your data input'})
+
         # Verifica se o pass está correto / existe
         if not user.check_password(password):
-            raise exceptions.AuthenticationFailed({'error': 'Please, verify your data input'})        
-    
+            raise exceptions.AuthenticationFailed(
+                {'error': 'Please, verify your data input'})
+
         # Chama o método ESTATICO que Cria o Token, passando o Id do usuário da Requisição
-        token = JWTAuthentucation.generate_jwt(user.id) 
+        token = JWTAuthentucation.generate_jwt(user.id)
         # PADRONIZAR AS RESPOSTAS PARA QUE FIQUEM ASSIM
-        return Response([ 
-                    {
-                        'message': 'success', 
+        return Response([
+            {
+                        'message': 'success',
                         # Retornando o Token no Login
                         'token': token,
                         'user': UserSerializer(user).data
-                    }
-                ], status=status.HTTP_200_OK )
-
+                        }
+        ], status=status.HTTP_200_OK)
 
 
 class AuthenticatedUserAPIView(APIView):
