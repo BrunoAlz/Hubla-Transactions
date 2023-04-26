685fa627c7cd0551209f08e61cd6459096df1622
Bruno Alves <bruno6821@gmail.com>
2023-04-26 03:37:49 -0300

    Implementação do Método de Authenticação dos Users

--- backend/administrator/authentication.py
+++ backend/administrator/authentication.py
@@ -1,12 +1,48 @@
 import jwt, datetime
+from rest_framework import exceptions
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from core.models import User
 
 class JWTAuthentucation:
     """
-    Criando o JWT utilizando a Lib: https://pyjwt.readthedocs.io/en/stable/
+    Cria o Token JWT utilizando a Lib: https://pyjwt.readthedocs.io/en/stable/
+    Authentica o usuário utilizando a função Authenticate
     """
+    def authenticate(self, request):
+        """
+            Recebe o token na requisição do usuário se authentica, permite que ele
+            acesse as views authenticadas.      
+        """
+        # Obter o token JWT da Requisição
+        auth_header = get_authorization_header(request).decode('utf-8')
+        if not auth_header or 'Bearer' not in auth_header:
+            return None
+        token = auth_header.split(' ')[1]
+
+        # Tenta Decodificar o Token e extrair o id do usuário
+        try:
+            payload = jwt.decode(token, "CHAVE_SECRETA_LEMBRAR_DE_MUDAR", algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise exceptions.AuthenticationFailed('Token expirado.')
+        except:
+            raise exceptions.AuthenticationFailed('Token inválido.')
+        # Extrai o id do usuário
+        user_id = payload['user_id']
+
+        # Faz a query no banco para para tentar buscar o usuário
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            raise exceptions.AuthenticationFailed('Usuário não encontrado.')
+
+        return (user, None)
+
     # Método para gerar o TOKEN 
     @staticmethod
     def generate_jwt(id):
+        """
+            Método para gerar o JWT https://pyjwt.readthedocs.io/en/stable/#example-usage
+        """
         #Cria o Payload para montar o JTW
         payload = {
             # Passa o Id do Usuário
