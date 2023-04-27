35f462f316a40fdc66fe7678937d9a883e3df480
Bruno Alves <bruno6821@gmail.com>
2023-04-27 20:44:48 -0300

    Separando o BSTP em componentes

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Navbar from '../../components/Navbar';
+import Sidemenu from "../../components/Sidemenu";
+
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="container-fluid">
+        <div className="row">
+          <Sidemenu />
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <h2>Section title</h2>
+            <div className="table-responsive">
+              <table className="table table-striped table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1,001</td>
+                    <td>random</td>
+                    <td>data</td>
+                    <td>placeholder</td>
+                    <td>text</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home
