dce94ea61c1e2fd843ecd4e514e34ad69f91afcf
Bruno Alves <bruno6821@gmail.com>
2023-04-28 03:13:16 -0300

    Ajustes no Main, remoção do Store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// Redux
-import { Provider } from "react-redux";
-import { store } from "../store/index.js";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
