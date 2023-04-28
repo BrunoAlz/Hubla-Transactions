dc2190f28eafd9cac54741dec5366f0a125105b4
Bruno Alves <bruno6821@gmail.com>
2023-04-28 01:10:25 -0300

    add Layout, CSS e dados do Login e Registro

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,43 +1,59 @@
-import React from 'react'
 import "./Auth.css";
-
+import logo from "../../assets/navLogo.svg";
 
 const Login = () => {
   return (
-    <div>
-      <form className="form-auth">
-        {/* <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> */}
-        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+    <div className="grey-bg px-4 mt-5 py-5 px-md-5 text-center text-lg-start">
+      <div className="container mt-5">
+        <div className="row gx-lg-5 align-items-center">
+          <div className="col-lg-6 mb-5 mb-lg-0">
+            <img
+              className="img-fluid hubla-login"
+              src={logo}
+              alt="Logo do Breaking News"
+            />
+            <h1 className="display-3 fw-bold ls-tight">
+              The best offer <br />
+              <span className="text-primary">for your {" "}
+              <span className="">.</span>
+              <span className="">T</span>
+              <span className="ms-4">x</span>
+              <span className="ms-4">t</span>
+              </span>
+            </h1>
+          </div>
 
-        <div className="form-floating">
-          <input
-            type="email"
-            className="form-control"
-            id="floatingInput"
-            placeholder="name@example.com"
-          />
-          <label for="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input
-            type="password"
-            className="form-control"
-            id="floatingPassword"
-            placeholder="Password"
-          />
-          <label for="floatingPassword">Password</label>
-        </div>
+          <div className="col-lg-6 mb-5 mb-lg-0">
+            <div className="card login-card shadow">
+              <div className="card-body py-5 px-md-5">
+                <form>
+                  <p className="text-center fs-2 fw-bold">Login</p>
+                  <div className="row mt-5">
+                    <div className="form-outline fs-5 mb-2  fw-semi-bold">
+                      <p>Email</p>
+                      <input type="email" className="form-control" />
+                    </div>
 
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-          </label>
+                    <div className="col-md-12 mb-4 mt-3">
+                      <div className="form-outline fs-5  ">
+                        <p>Password</p>
+                        <input type="password" className="form-control" />
+                      </div>
+                    </div>
+
+                    <button
+                      type="submit"
+                      className="btn btn-primary btn-block mb-4 shadow"
+                    >
+                      <span className="fs-5">Sign in</span>
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Sign in
-        </button>
-        <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
-      </form>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,43 +1,93 @@
-import React from 'react'
 import "./Auth.css";
-
+import logo from "../../assets/navLogo.svg";
 
 const Register = () => {
   return (
-    <div>
-      <form className="form-auth">
-        {/* <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> */}
-        <h1 className="h3 mb-3 fw-normal">Register</h1>
+    <div className="grey-bg px-4 mt-5 py-5 px-md-5 text-center text-lg-start">
+      <div className="container mt-5">
+        <div className="row gx-lg-5 align-items-center">
+          <div className="col-lg-6 mb-5 mb-lg-0">
+            <div className="card">
+              <div className="card-body py-5 px-md-5 shadow">
+                <p className="text-center fs-2 fw-bold">Register</p>
+                <form>
+                  <div className="row">
+                    <div className="col-md-6 mb-4">
+                      <div className="form-outline">
+                        <label className="form-label fs-5 fw-semi-bold">
+                          First name
+                        </label>
+                        <input type="text" className="form-control" />
+                      </div>
+                    </div>
+                    <div className="col-md-6 mb-4">
+                      <div className="form-outline">
+                        <label className="form-label fs-5 fw-semi-bold">
+                          Last name
+                        </label>
+                        <input type="text" className="form-control" />
+                      </div>
+                    </div>
+                  </div>
 
-        <div className="form-floating">
-          <input
-            type="email"
-            className="form-control"
-            id="floatingInput"
-            placeholder="name@example.com"
-          />
-          <label for="floatingInput">Email address</label>
-        </div>
-        <div className="form-floating">
-          <input
-            type="password"
-            className="form-control"
-            id="floatingPassword"
-            placeholder="Password"
-          />
-          <label for="floatingPassword">Password</label>
-        </div>
+                  <div className="form-outline mb-4">
+                    <label className="form-label fs-5 fw-semi-bold">
+                      Email
+                    </label>
+                    <input type="password" className="form-control" />
+                  </div>
+                  <div className="row">
+                    <div className="col-md-6 mb-4">
+                      <div className="form-outline">
+                        <label className="form-label fs-5 fw-semi-bold">
+                          Password
+                        </label>
+                        <input type="text" className="form-control" />
+                      </div>
+                    </div>
+                    <div className="col-md-6 mb-4">
+                      <div className="form-outline">
+                        <label className="form-label fs-5 fw-semi-bold">
+                          Confirm Password
+                        </label>
+                        <input type="text" className="form-control" />
+                      </div>
+                    </div>
+                  </div>
 
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-          </label>
+                  <div className="row">
+                    <button
+                      type="submit"
+                      className="btn btn-primary btn-block shadow mb-4"
+                    >
+                      <span className="fs-5">Sign up</span>
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-6 mb-lg-0">
+            <img
+              className="hubla-register"
+              src={logo}
+              alt="Logo do Breaking News"
+            />
+            <h1 className="display-5 mb-4 fw-bold ls-tight">Transactions</h1>
+            <p className="text-primary display-4">
+              Your <span className="fw-bold">.</span>TXT will now work for you.
+            </p>
+            <p className="login-text pt-3 mt-4">
+              With our data extraction software, turning your{" "}
+              <span className="fw-bold">.</span>TXT into cash is easier than
+              printing cash at home. Don't waste any more time trying to
+              decipher your financial data manually. Let us take care of it for
+              you and unlock the full potential of your business.
+            </p>
+          </div>
         </div>
-        <button className="w-100 btn btn-lg btn-primary" type="submit">
-          Register
-        </button>
-        <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
-      </form>
+      </div>
     </div>
   );
 };
