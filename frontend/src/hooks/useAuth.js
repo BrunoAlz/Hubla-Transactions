6d339ccc8541ccf4d25cae73d55c7dbf1a4344df
Bruno Alves <bruno6821@gmail.com>
2023-04-29 00:22:37 -0300

    Removendo console.log

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -3,17 +3,14 @@ import { useSelector } from "react-redux";
 
 export const useAuth = () => {
   const { user } = useSelector((state) => state.auth);
-  console.log(user)
 
   const [auth, setAuth] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (user) {
-      console.log("AUTHHHHH TRUEEE");
       setAuth(true);
     } else {
-      console.log("AUTHHHHH FALSE");
       setAuth(false);
     }
 
