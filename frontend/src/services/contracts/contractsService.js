c2de9c4ebcb39cf2ea31b8adaf401257bfabe275
Bruno Alves <bruno6821@gmail.com>
2023-04-30 03:35:48 -0300

    Service para buscar os detalhes do Contrato

--- frontend/src/services/contracts/contractsService.js
+++ frontend/src/services/contracts/contractsService.js
@@ -32,9 +32,26 @@ const getContracts = async () => {
       },
     }
   );
-  console.log(response);
   return response;
 };
 
-const contractsService = { contractUpload, getContracts };
+const getContractTransactions = async (contract_id) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const response = await axios.get(
+    API_ENDPOINT + "transactions/contract/list/",
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${user.token}`,
+      },
+    }
+  );
+  return response;
+};
+
+const contractsService = {
+  contractUpload,
+  getContracts,
+  getContractTransactions,
+};
 export default contractsService;
