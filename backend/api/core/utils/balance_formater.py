2655eeb8b746d17d93b6a7bf4d562e47bc52ee74
Bruno Alves <bruno6821@gmail.com>
2023-05-03 02:25:18 -0300

    Excluindo arquivos inutilizados

--- backend/api/core/utils/balance_formater.py
+++
@@ -1,4 +0,0 @@
-def balance_formater(balance):
-    """Converte um valor em centavos para reais formatados."""
-    formated_balance = float(balance) / 100.0
-    return f'R$ {formated_balance:.2f}'
