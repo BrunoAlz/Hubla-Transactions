52540c4839ee01a612e940dab7039d29c3b125a4
Bruno Alves <bruno6821@gmail.com>
2023-05-03 05:16:38 -0300

    Instalando configurações para testes

---
+++ backend/api/tests/transactions/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+
+class TransactionsUrlsTest(TestCase):
+    def test_contract_create_url(self):
+        assert 1 == 2
--- backend/configurations/settings.py
+++ backend/configurations/settings.py
@@ -91,9 +91,12 @@ DATABASES = {
         'PORT': config('DB_PORT'),
         "OPTIONS": {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-            'charset': 'utf8mb3',
+            'charset': 'utf8',
             "autocommit": True,
-        }
+        },
+        'TEST': {
+            'NAME': 'teste',
+        },
     }
 }
 
